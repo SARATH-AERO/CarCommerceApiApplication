afead0e9734a2d41a03594f4a59a464e0572ef25
Sarath S <sarath-s@hcltech.com>
2024-11-11 07:30:29 +0000

    ResponseCarDto.java edited online with Bitbucket

--- src/main/java/com/hcltech/car_commerce_api/dto/ResponseCarDto.java
+++ src/main/java/com/hcltech/car_commerce_api/dto/ResponseCarDto.java
@@ -1,2 +1,34 @@
-package com.hcltech.car_commerce_api.dto;public class ResponseCarDto {
+package com.hcltech.car_commerce_api.dto;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class ResponseCarDto {
+
+    private Integer id;
+    private String carName;
+    private String brand;
+    private String model;
+    private Integer manufacturerYear;
+    private String colour;
+    private double price;
+    private String engineNumber;
+    private Integer seatCapacity;
+    private String fuelType;
+    private String transmission;
+    private float mileage;
+    private String description;
+    private String kmDriven;
+    private String location;
+
+    private String carVariant;
+
+    private String hub;
+
 }
