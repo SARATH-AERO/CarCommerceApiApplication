46831d67b7c8b28478c435706592544b0d1eb2e2
Sarath S <sarath-s@hcltech.com>
2024-11-11 07:28:22 +0000

    CarDto.java edited online with Bitbucket

--- src/main/java/com/hcltech/car_commerce_api/dto/CarDto.java
+++ src/main/java/com/hcltech/car_commerce_api/dto/CarDto.java
@@ -23,4 +23,24 @@ public class CarDto
     @NotBlank(message = "EngineNumber cannot be empty")
     @Size(min = 17, max = 17, message = "EngineNumber must be exactly 17 characters long")
     private String engineNumber;
+    
+    // new fields
+
+    private Integer seatCapacity;
+
+    private String fuelType;
+
+    private String transmission;
+
+    private float mileage;
+
+    private String description;
+
+    private String kmDriven;
+
+    private String location;
+
+    private String carVariant;
+
+    private String hub;
 }
